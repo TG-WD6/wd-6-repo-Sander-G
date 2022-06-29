1f7fa04f07b51344bed03e0d31648a6eaee9609d
Sander_iMac <sander@grunnekemeijer.net>
2022-06-29 17:04:52 +0200

    carousel v0.8

--- 02_Programming/Javascript/eindopdracht/image_carousel/script.js
+++ 02_Programming/Javascript/eindopdracht/image_carousel/script.js
@@ -11,13 +11,30 @@ const imgSize = images[0].clientWidth
 inner_carousel.style.transform = `translateX(${-imgSize}px)`;
 
 prevButton.addEventListener('click', () => {
+    if (current <= 0) return;
     inner_carousel.style.transition = '200ms ease-in-out transform';
     current--;
     inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
 })
 
 nextButton.addEventListener('click', () => {
+    if (current >= images.length - 1) return;
     inner_carousel.style.transition = '200ms ease-in-out transform';
     current++;
     inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
+}) 
+
+inner_carousel.addEventListener('transitionend', () => {
+    if (images[current].classList.contains('first_img')) {
+        inner_carousel.style.transition = 'none';
+        current = images.length - 2;
+        inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
+        }
+
+    if (images[current].classList.contains('last_img')) {
+        inner_carousel.style.transition = 'none';
+        current = images.length - current;
+        inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
+        }
 })
+
