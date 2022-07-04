2efd269498721b1a565088712216e20e97d5a515
Sander_iMac <sander@grunnekemeijer.net>
2022-07-04 18:25:16 +0200

    JSONcards update, nog card styling

---
+++ 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
@@ -0,0 +1,34 @@
+const data = ('./data.json');
+const fetchTransformer = () => {
+  fetch('./data.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // for (let i = 0; i < data.length; i++)
+      // console.log(data);
+    
+
+      appendData(data);
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+
+function appendData(data) {
+const mainContainer = document.getElementById('transformerDex');
+for (let i = 0; i < data.length; i++) {
+  const div = document.createElement('div');
+  div.innerHTML = 'Name: ' + data[i].Name + '   ' + 'Allegiance: ' + data[i].Allegiance ; 
+
+  mainContainer.appendChild(div);
+}
+
+}   
+
+
+
+
+}
+fetchTransformer();
+
