4aef99b87a657c93ddbb08d90d7b6473ab28ebf3
Sander_iMac <sander@grunnekemeijer.net>
2022-07-05 18:00:31 +0200

    transformer-dex nog modal afmaken

--- 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
+++ 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
@@ -7,7 +7,7 @@ const fetchTransformer = () => {
     .then(function (data) {
       // for (let i = 0; i < data.length; i++)
       // console.log(data);
-    
+
 
       appendData(data);
     })
@@ -15,20 +15,45 @@ const fetchTransformer = () => {
       console.log(err);
     })
 
-function appendData(data) {
-const mainContainer = document.getElementById('transformerDex');
-for (let i = 0; i < data.length; i++) {
-  const div = document.createElement('div');
-  div.innerHTML = 'Name: ' + data[i].Name + '   ' + 'Allegiance: ' + data[i].Allegiance ; 
+  function appendData(data) {
+    const mainContainer = document.getElementById('container');
+    for (let i = 0; i < data.length; i++) {
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('card__wrapper');
+      // add class to wrapper
+      const title = document.createElement('h2');
+      title.innerHTML = data[i].Name;
+      const allegiance = document.createElement('h3');
+      allegiance.innerHTML = 'Allegiance: ' + data[i].Allegiance;
+      const subgroup = document.createElement('p');
+      subgroup.innerHTML = 'Subgroup: ' + data[i].Subgroup;
+      const img = document.createElement('img');
+      img.classList.add('transformer__image');
+      img.src = data[i].imgURL;
+      img.style.width = "251px";
+      mainContainer.appendChild(wrapper);
+      wrapper.append(title, allegiance, subgroup, img);
 
-  mainContainer.appendChild(div);
-}
 
-}   
+    }
 
-
-
+  }
 
 }
 fetchTransformer();
+
+// const images = document.querySelectorAll(".transformer__image img");
+// const modal = document.querySelector(".modal");
+// const modal__img = document.queryselector(".modal__img");
+// const close = document.querySelector(".close");
+
+// images.forEach((image) => {
+//   image.addEventListener("click", () => {
+//     modal.classList.add("appear");
+
+//   });
+// })
+
+
+
 
