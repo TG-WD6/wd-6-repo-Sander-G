2ace03e4204cc074d68bccd03743f26d39548302
Sander_iMac <sander@grunnekemeijer.net>
2022-07-12 16:46:59 +0200

    searchbar almost there

--- 02_Programming/Javascript/eindopdracht/Searchbar/app.js
+++ 02_Programming/Javascript/eindopdracht/Searchbar/app.js
@@ -1,50 +1,96 @@
-const form = document.querySelector("form");
-const input = document.getElementById("searchInput");
-const ul = document.querySelector("ul");
-const searchArray = [];
-
-localStorage.setItem('items', JSON.stringify(searchArray));
-const data = JSON.parse(localStorage.getItem('items'));
+const form = document.querySelector('form');                      // form querySelector
+const input = document.getElementById('searchInput');             // user input constant
+const output = document.querySelector('.output')                  // div voor weergave zoek items
+const ul = document.getElementById('listMaker');                  // Lijst voor ListMaker
 
 
+let searchArray = [];                                             // declaratie searchArray voor ingevoerde zoektermen
+localStorage.setItem('items', JSON.stringify(searchArray));       // searchArray items in localstorage
+const data = JSON.parse(localStorage.getItem('items'));           // items uit localstorage halen
 
-function liMaker(text) {
+
+
+
+
+function listMaker(text) {                                        // user input toevoegen aan listmaker lijst
     const li = document.createElement('li');
     li.textContent = text;
     ul.appendChild(li);
 }
 
-form.addEventListener('submit', function (event) {
-   event.preventDefault(); // prevent default submit action
-   searchArray.push(input.value); // user input toevoegen aan search Array
-   localStorage.setItem('items', JSON.stringify(searchArray)); // javascript object naar string omzetten
+form.addEventListener('submit', function (e) {                    // event listener voor submit van zoekterm
+   e.preventDefault();                                            // prevent default submit action (..iets met geen server)
+   searchArray.push(input.value);                                 // user input toevoegen aan search Array on submit
+   localStorage.setItem('items', JSON.stringify(searchArray));    // javascript search array object naar string omzetten on submit
    
    
-   liMaker(input.value); // voeg item toe aan front end lijst
-    input.value = ''; // reset input field na submit
-  })
-
-
-
- // filter om input te vergelijken met local storage 
-  input.addEventListener("input", function () {
-    let result = [];
-
-    if (input.value) {
-        result = JSON.parse(localStorage.getItem("items")).filter(
-            function (searchWord) {
-                if (searchWord.toUpperCase().includes(input.value.toUpperCase())) {
-                    return searchWord;
-                }
-            else return;
-    });    
-    }
-
+   listMaker(input.value);                                        // voeg item toe aan listMaker lijst
+    input.value = '';                                             // reset inputfield na submit
   });
 
+input.addEventListener('input', filter);                          // event listener op user input voor filter
+
+function filter (ev) {
+    // console.log(ev.target.value.toLowerCase());                // capture wat de gebruiker invult per keystroke
+    let temp = '';  
+    const result = searchArray.filter(function (items) {
+      return items.toLowerCase().includes(ev.target.value.toLowerCase());
+      });
+    
+      // console.log(result)                                          // log resultaat van vergelijking van input met searcharray items 
+
+    if(result.length > 0 ) {                                              // als resultaat van vergl gevonden dan geef voor ieder
+      temp = `<ul class='search-items'>`;                              // item een list item in search-items lijst
+      result.forEach( (item) => {
+        temp +=  `<li class='search-item'> ${item} </li>`;
+      });
+      temp += `</ul>`;
+    }
+    else{
+      temp =`<li class='search-item'></li>`;                            // anders geef leeg list item
+    }
+    output.innerHTML = temp;                                            // output html 
+}
+
+input.addEventListener("input", function(event){                        // nog niet werkende event
+    if (input.value.length = 0 ) {                                      // listener om zoekwoorden te 
+      function changeStyle(){                                           // verbergen bij lege searchbar
+        const element = document.getElementsByClassName("output");
+        element.style.visibility = "hidden";
+      }
+    }
+    else {
+      function changeStyle(){
+        const element = document.getElementsByClassName("output");
+        element.style.visibility = "visible";
+      }
+    }
+
+})
 
 
-//zoek suggesties weergeven tijdens input ahv filter
+//
+//       
+//    TODO:     
+//
+
+// - list items verbergen als er geen input in input is.
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
