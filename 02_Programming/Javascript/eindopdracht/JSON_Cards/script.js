711bef261cbc235c139adeb5448373ef884ff0b3
Sander <sander@grunnekemeijer.net>
2022-07-13 21:23:27 +0200

    JSON Modal revisited

--- 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
+++ 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
@@ -38,9 +38,21 @@ const fetchTransformer = () => {
 fetchTransformer();
 
 
+const images = document.querySelectorAll("img");  // ???? 
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const close = document.querySelector(".close");
 
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    modalImg.src = img.src;
+    modal.classList.add("appear");
 
-
+    close.addEventListener("click", () => {
+      modal.classList.remove("appear");
+    });
+  });
+});
 
 
 
