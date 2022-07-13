9c5b7e71f399df1325ddcd3336cc8011e91aa2da
Sander <sander@grunnekemeijer.net>
2022-07-13 21:46:01 +0200

    carousel tv photoshopgrade

--- 02_Programming/Javascript/eindopdracht/image_carousel/script.js
+++ 02_Programming/Javascript/eindopdracht/image_carousel/script.js
@@ -10,24 +10,26 @@ let current = 1;
 const imgSize = images[0].clientWidth
 inner_carousel.style.transform = `translateX(${-imgSize}px)`;
 
-prevButton.addEventListener('click', () => {
-    if (current <= 0) return;
-    inner_carousel.style.transition = '200ms ease-in-out transform';
-    current--;
-    inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
-})
+prevButton.addEventListener('click', function () {
+        if (current <= 0)
+            return;
+        inner_carousel.style.transition = '200ms ease-in-out transform';
+        current--;
+        inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
+    })
 
-nextButton.addEventListener('click', () => {
-    if (current >= images.length - 1) return;
-    inner_carousel.style.transition = '200ms ease-in-out transform';
-    current++;
-    inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
-}) 
+nextButton.addEventListener('click', function () {
+        if (current >= images.length - 1)
+            return;
+        inner_carousel.style.transition = '200ms ease-in-out transform';
+        current++;
+        inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
+    }) 
 
 inner_carousel.addEventListener('transitionend', () => {
     if (images[current].classList.contains('first_img')) {
         inner_carousel.style.transition = 'none';
-        current = images.length - 2;
+        current = images.length - 1;
         inner_carousel.style.transform = `translateX(${-imgSize * current}px)`;
         }
 
