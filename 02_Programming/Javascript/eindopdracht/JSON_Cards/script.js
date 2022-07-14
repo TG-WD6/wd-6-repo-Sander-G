92b56b6e723b1296ba488555960ece38dacfdb1b
Sander_iMac <sander@grunnekemeijer.net>
2022-07-14 16:56:20 +0200

    JSON cards met geluid

--- 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
+++ 02_Programming/Javascript/eindopdracht/JSON_Cards/script.js
@@ -1,14 +1,12 @@
-
-
 const data = ('./files/data.json');
 const fetchTransformer = () => {
   fetch('./files/data.json')
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-      // for (let i = 0; i < data.length; i++)
-      // console.log(data);
+    .then(function (data) {                                                 // loggen data voor alle items:
+                                                                            // for (let i = 0; i < data.length; i++)
+                                                                            // console.log(data);
 
       appendData(data);
     })
@@ -16,11 +14,13 @@ const fetchTransformer = () => {
       console.log(err);
     })
 
+
+
   function appendData(data) {
     const mainContainer = document.getElementById('container');
     for (let i = 0; i < data.length; i++) {
-      const wrapper = document.createElement('div');                                            // wrapper div toevoegen
-      wrapper.classList.add('card__wrapper');                                                   // add class to wrapper 
+      const wrapper = document.createElement('div');                         // wrapper div toevoegen
+      wrapper.classList.add('card__wrapper');                                // add class to wrapper 
       const title = document.createElement('h2');
       title.innerHTML = data[i].Name;
       const allegiance = document.createElement('h3');
@@ -30,20 +30,16 @@ const fetchTransformer = () => {
       const img = document.createElement('img');
       img.classList.add('modal__image');
       img.src = data[i].imgURL;
-      img.style.width = "251px";
-      mainContainer.appendChild(wrapper);                                                       // wrapper aan container div koppelen
-      wrapper.append(title, allegiance, subgroup, img);                                         // title, allegiance, subgroup, img aan wrapper toevoegen     
+      img.style.width = "253px";
+      mainContainer.appendChild(wrapper);                                      // wrapper aan container div koppelen
+      wrapper.append(title, allegiance, subgroup, img);                        // title, allegiance, subgroup, img aan wrapper toevoegen     
+    
     }
-
-
-
-
-
+    const modalContent = document.querySelector(".modal__content");
     const image = document.querySelectorAll(".modal__image");
     const modal = document.querySelector(".modal");
     const close = document.querySelector(".close");
-    const modalContent = document.querySelector(".modal__content")
-    const modalPlaatje = document.querySelector(".modal__plaatje");
+    const modalPic = document.querySelector(".modal__pic");
 
     modal.style.opacity = "0";
     modal.style.zIndex = "-1";
@@ -51,23 +47,30 @@ const fetchTransformer = () => {
 
     image.forEach((image) => {
       image.addEventListener("click", (e) => {
-        console.log(e.target.src);
-        modalPlaatje.src = e.target.src;
+        modalPic.src = e.target.src;
         modal.style.opacity = "1";
         modal.style.zIndex = "1";
         modal.pointerEvents = "all";
+        document.getElementById("transform").play();
       });
+
+
+
     });
 
     close.addEventListener("click", (e) => {
       modal.style.opacity = "0";
       modal.style.zIndex = "-1";
       modal.pointerEvents = "none";
-      console.log(e)
     });
 
+  
+
   }
+
+  
 };
+
 fetchTransformer();
 
 
@@ -76,11 +79,17 @@ fetchTransformer();
  //    Achtergrondkleuren    //
 //////////////////////////////
 
-// None: '#fceaff',
-// Autobot: '#ff3333',
-// Decepticon: '#f8d5a3',
-// Quintesson: '#97b3e6',
-// Unicron: '#eaeda1',
+// const BGcolor = colors[allegiance];
+// const card__wrapper = document.querySelector(".card__wrapper");
+// card__wrapper.style.backgroundColor = BGcolor;
+
+// const colors = {
+//   None: '#fceaff',
+//   Autobot: '#ff3333',
+//   Decepticon: '#f8d5a3',
+//   Quintesson: '#97b3e6',
+//   Unicron: '#eaeda1',
+// }
 
 
 
